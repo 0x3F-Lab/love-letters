590996a2a5416f404d7031ed5c5f53dfaf262fb9
Alex Hawking <alexhawking23@gmail.com>
2024-04-17 15:22:16 +0800

    fix logic for posting and redirects

--- src/blueprints/post.py
+++ src/blueprints/post.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from models import Post, db
 
 post = Blueprint("post", __name__)
 
 
-@post.route("/create", methods=["POST"])
-def create():
+@post.route("/create_post", methods=["POST"])
+def create_post():
     if request.method == "POST":
         user_id = session.get("user_id")
         if not user_id:
@@ -40,8 +40,16 @@ def create():
                 f"ID: {post.post_id}, Title: {post.title}, Content: {post.content}, Type: {post.post_type}, Poster: {post.user_id}, Anonymous: {post.is_anonymous}, Time: {post.created_at}"
             )
 
-        return redirect(url_for("browse"))
+        return redirect(url_for("post.browse"))
 
+
+@post.route("/create")
+def create():
+    user_id = session.get("user_id")
+    if not user_id:
+        flash("You need to login to post.", "danger")
+        return redirect(url_for("auth.login"))
+    return render_template("post.html")
 
 @post.route("/browse")
 def browse():
