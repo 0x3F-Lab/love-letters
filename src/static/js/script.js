590d73cbae44565a4cec0b820573660f2d679e45
Alex-Hawking <Alex-Hawking@users.noreply.github.com>
2024-04-19 13:35:14 +0000

    Database updated and code formatted [skip ci]

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -12,22 +12,28 @@ $(document).ready(function () {
 
 function loadUserProfile(userId) {
   fetch(`/profile/${userId}`)
-      .then(response => response.json())
-      .then(data => {
-          // Clear previous data
-          document.getElementById('user-name').textContent = `${data.first_name} ${data.last_name}`;
-          document.getElementById('user-email').textContent = data.email;
-          document.getElementById('user-phone').textContent = data.phone_number || 'N/A';
+    .then((response) => response.json())
+    .then((data) => {
+      // Clear previous data
+      document.getElementById("user-name").textContent =
+        `${data.first_name} ${data.last_name}`;
+      document.getElementById("user-email").textContent = data.email;
+      document.getElementById("user-phone").textContent =
+        data.phone_number || "N/A";
 
-          // Checking if the social media information exists
-          document.getElementById('user-instagram').textContent = data.socials.instagram || 'N/A';
-          document.getElementById('user-facebook').textContent = data.socials.facebook || 'N/A';
-          document.getElementById('user-snapchat').textContent = data.socials.snapchat || 'N/A';
-          document.getElementById('user-twitter').textContent = data.socials.twitter || 'N/A';
-      })
-      .catch(error => {
-          console.error('Error loading the user profile:', error);
-          const modalBody = document.querySelector('#profileModal .modal-body');
-          modalBody.innerHTML = `<p>Error loading profile. Please try again.</p>`; // Handle loading errors
-      });
+      // Checking if the social media information exists
+      document.getElementById("user-instagram").textContent =
+        data.socials.instagram || "N/A";
+      document.getElementById("user-facebook").textContent =
+        data.socials.facebook || "N/A";
+      document.getElementById("user-snapchat").textContent =
+        data.socials.snapchat || "N/A";
+      document.getElementById("user-twitter").textContent =
+        data.socials.twitter || "N/A";
+    })
+    .catch((error) => {
+      console.error("Error loading the user profile:", error);
+      const modalBody = document.querySelector("#profileModal .modal-body");
+      modalBody.innerHTML = `<p>Error loading profile. Please try again.</p>`; // Handle loading errors
+    });
 }
