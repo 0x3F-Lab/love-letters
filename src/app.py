ca8a9390e2eaa0c17f4c8ac830ca8d04df6ab88d
Connor2803 <87635341+Connor2803@users.noreply.github.com>
2024-04-29 17:54:33 +0800

    Add DB migration
    Using Flask-Migrate, added an initial migration, and then a version with the "is_anonymous" column added.

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_migrate import Migrate
 from models import db
 from config import DevelopmentConfig
 
@@ -7,12 +8,15 @@ from blueprints.auth import auth
 from blueprints.post import post
 
 
+
 def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    migrate = Migrate(app,db)
 
     db.init_app(app)
-
+    migrate.init_app(app,db)
+    
     # Register Blueprints with their URL prefixes
     app.register_blueprint(auth, url_prefix="/auth")
     app.register_blueprint(post, url_prefix="/post")
